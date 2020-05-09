71e1d7b4828827f390fef2723f827005d272d917
dasitha <dasisandaruwan@gmail.com>
2020-05-09 14:41:26 +0530

    added the basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'HIIIIIIIIIII';
+  loadedFeature = 'recipe';
+  onNavigate(feature:string){
+      this.loadedFeature = feature;
+  }
 }
